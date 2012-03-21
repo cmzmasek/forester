790849e542e581631f0b757becfba72a5f6f1b58
cmzmasek@gmail.com <cmzmasek@gmail.com@ca865154-3058-d1c3-3e42-d8f55a55bdbd>
2012-03-21 04:23:35 +0000

    in progress

--- forester/ruby/scripts/preprocess.rb
+++ forester/ruby/scripts/preprocess.rb
@@ -1,9 +1,16 @@
 #!/usr/local/bin/ruby -w
 
 
+if ( ARGV == nil || ARGV.length != 3 )
+  puts( "usage: preprocess.rb <input> <path to Pfam A HMMs>" )         
+  exit( -1 )
+end 
+
 input = ARGV[ 0 ]
 
-system( "hmmscan --nobias --domtblout " + input + "_hmmscan_260_10 -E 10 Pfam-A.hmm " + input + ".ni.fasta" )
+pfam = ARGV[ 1 ]
+
+system( "hmmscan --nobias --domtblout " + input + "_hmmscan_260_10 -E 10 " + pfam +  " + input + ".ni.fasta" )
 
 system( "hsp " + input + "_hmmscan_260_10 " + input + "_hmmscan_260_10_domain_table" )
 
