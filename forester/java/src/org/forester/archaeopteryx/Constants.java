aa5f51c346521014007587c905c16bed9f1d971c
cmzmasek@gmail.com <cmzmasek@gmail.com@ca865154-3058-d1c3-3e42-d8f55a55bdbd>
2012-03-29 01:01:55 +0000

    in progress

--- forester/java/src/org/forester/archaeopteryx/Constants.java
+++ forester/java/src/org/forester/archaeopteryx/Constants.java
@@ -42,7 +42,7 @@ public final class Constants {
     public final static boolean __SYNTH_LF                                                    = false;                                                    // TODO remove me
     public final static boolean ALLOW_DDBJ_BLAST                                              = false;
     public final static String  PRG_NAME                                                      = "Archaeopteryx";
-    final static String         VERSION                                                       = "0.970 9M";
+    final static String         VERSION                                                       = "0.970 BG";
     final static String         PRG_DATE                                                      = "2012.03.28";
     final static String         DEFAULT_CONFIGURATION_FILE_NAME                               = "_aptx_configuration_file";
     final static String[]       DEFAULT_FONT_CHOICES                                          = { "Verdana", "Tahoma",
