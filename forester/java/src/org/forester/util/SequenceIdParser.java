978fa1fa2b3cad823dd117d8281bf50aab8f205f
cmzmasek <cmzmasek@ca865154-3058-d1c3-3e42-d8f55a55bdbd>
2012-06-06 13:53:24 +0000

    phylotastic hackathon at NESCENT 120606

---
+++ forester/java/src/org/forester/util/SequenceIdParser.java
@@ -0,0 +1,48 @@
+// $Id:
+// FORESTER -- software libraries and applications
+// for evolutionary biology research and applications.
+//
+// Copyright (C) 2008-2009 Christian M. Zmasek
+// Copyright (C) 2008-2009 Burnham Institute for Medical Research
+// Copyright (C) 2000-2001 Washington University School of Medicine
+// and Howard Hughes Medical Institute
+// Copyright (C) 2003-2007 Ethalinda K.S. Cannon
+// All rights reserved
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+//
+// Contact: phylosoft @ gmail . com
+// WWW: www.phylosoft.org/forester
+
+package org.forester.util;
+
+public final class SequenceIdParser {
+
+    
+    // ref_XP_002434188_1_mites___ticks_
+    // ref_NP_001121530_1_frogs___toads_
+    // gb_ADF31344_1_segmented_worms_
+    // gb_AAA96518_1
+    // gb_EHB07727_1_rodents_
+    // dbj_BAF37827_1_turtles_
+    // emb_CAA73223_1_primates_
+    // lcl_91970_unknown_
+    
+    
+    
+    private SequenceIdParser() {
+        // Hiding the constructor.
+    }
+}
