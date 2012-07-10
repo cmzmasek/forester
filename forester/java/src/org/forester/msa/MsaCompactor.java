52b0831883cbde06095697f88527afa497112680
cmzmasek@gmail.com <cmzmasek@gmail.com@ca865154-3058-d1c3-3e42-d8f55a55bdbd>
2012-07-10 06:12:27 +0000

    in progress

--- forester/java/src/org/forester/msa/MsaCompactor.java
+++ forester/java/src/org/forester/msa/MsaCompactor.java
@@ -16,7 +16,7 @@ public class MsaCompactor {
     
     private double[] calcGappiness() {
         final double gappiness[] = new double[ _msa.getLength() ];
-        final int seqs =     _msa.getNumberOfSequences();
+        final int seqs =  _msa.getNumberOfSequences();
         for( int i = 0; i < gappiness.length; ++i ) {
             gappiness[ i ] = ( double ) MsaMethods.calcGapSumPerColumn( _msa, i )  / _msa.getNumberOfSequences();
             
