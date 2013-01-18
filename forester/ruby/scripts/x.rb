d309b294ee45478e45c38fef85bd00a2cae9b544
cmzmasek@gmail.com <cmzmasek@gmail.com@ca865154-3058-d1c3-3e42-d8f55a55bdbd>
2013-01-18 22:48:33 +0000

    inprogress

---
+++ forester/ruby/scripts/x.rb
@@ -0,0 +1,15 @@
+PRG = "/home/czmasek/SOFTWARE/FORESTER/DEV/forester/forester/ruby/evoruby/exe/hsp.rb"
+Dir.foreach(".") do |f|
+  next if f == '.' or f == '..'
+  if f =~ /^([A-Z0-9]{3,6})\.hmmscan_260/
+    taxon = $1
+    cmd = "#{PRG} -ie=10 -a -pe=1 -m=RRMa -s=#{taxon} #{taxon}.hmmscan_260 #{taxon}_ie10_domain_table > #{taxon}_ie10_summary_table.txt"
+    puts cmd
+    output = IO.popen( cmd )
+  elsif f =~ /^([A-Z0-9]{3,6})_cdhit090\.hmmscan_260/  
+    taxon = $1
+    cmd = "#{PRG} -ie=10 -a -pe=1 -m=RRMa -s=#{taxon} #{taxon}_cdhit090.hmmscan_260 #{taxon}_cdhit090_ie10_domain_table > #{taxon}_cdhit090_ie10_summary_table.txt"
+    puts cmd
+    output = IO.popen( cmd )
+  end  
+end
