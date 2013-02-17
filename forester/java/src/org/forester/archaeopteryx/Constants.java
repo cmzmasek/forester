436d91838f6c75966ac6a16e0ec25ab35f320e57
cmzmasek <cmzmasek@ca865154-3058-d1c3-3e42-d8f55a55bdbd>
2013-02-17 07:31:08 +0000

    in progress

--- forester/java/src/org/forester/archaeopteryx/Constants.java
+++ forester/java/src/org/forester/archaeopteryx/Constants.java
@@ -43,7 +43,7 @@ public final class Constants {
     public final static boolean ALLOW_DDBJ_BLAST                                              = false;
     public final static String  PRG_NAME                                                      = "Archaeopteryx";
     final static String         VERSION                                                       = "0.9802";
-    final static String         PRG_DATE                                                      = "130211";
+    final static String         PRG_DATE                                                      = "130216";
     final static String         DEFAULT_CONFIGURATION_FILE_NAME                               = "_aptx_configuration_file";
     final static String[]       DEFAULT_FONT_CHOICES                                          = { "Verdana", "Tahoma",
             "Arial", "Helvetica", "Dialog", "Lucida Sans", "SansSerif", "Sans-serif", "Sans" };
--- forester/java/src/org/forester/util/ForesterConstants.java
+++ forester/java/src/org/forester/util/ForesterConstants.java
@@ -27,8 +27,8 @@ package org.forester.util;
 
 public final class ForesterConstants {
 
-    public final static String  FORESTER_VERSION            = "1.020";
-    public final static String  FORESTER_DATE               = "130211";
+    public final static String  FORESTER_VERSION            = "1.021";
+    public final static String  FORESTER_DATE               = "130216";
     public final static String  PHYLO_XML_VERSION           = "1.10";
     public final static String  PHYLO_XML_LOCATION          = "http://www.phyloxml.org";
     public final static String  PHYLO_XML_XSD               = "phyloxml.xsd";
