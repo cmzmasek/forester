05157d0ba7145aebb34f4d3b6d4c38c380626b2b
cmzmasek@gmail.com <cmzmasek@gmail.com@ca865154-3058-d1c3-3e42-d8f55a55bdbd>
2013-03-30 00:22:34 +0000

    inprogress

--- forester/java/src/org/forester/archaeopteryx/TreePanel.java
+++ forester/java/src/org/forester/archaeopteryx/TreePanel.java
@@ -36,6 +36,7 @@ import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.RenderingHints;
+import java.awt.Stroke;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusAdapter;
@@ -4554,11 +4555,12 @@ public final class TreePanel extends JPanel implements ActionListener, MouseWhee
                 .setXSecondary( ( float ) ( getVisibleRect().x + getOvXPosition() + ( MOVE / ( getVisibleRect().width / getOvRectangle()
                         .getWidth() ) ) ) );
         _phylogeny.getRoot().setYSecondary( ( getVisibleRect().y + getOvYStart() ) );
+        final Stroke s = g.getStroke();
         g.setStroke( new BasicStroke( 0.5f ) ); //TODO
         for( final PhylogenyNode element : _nodes_in_preorder ) {
             paintNodeLite( g, element );
         }
-        g.setStroke( new BasicStroke( 1 ) ); //TODO
+        g.setStroke( s );
         paintOvRectangle( g );
     }
 
