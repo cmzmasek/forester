2a07984b81af7eb3c639ad81d4a7ef4504b0456b
cmzmasek@gmail.com <cmzmasek@gmail.com@ca865154-3058-d1c3-3e42-d8f55a55bdbd>
2013-11-28 04:24:30 +0000

    inprogress

--- forester/ruby/evoruby/lib/evo/tool/multi_sequence_extractor.rb
+++ forester/ruby/evoruby/lib/evo/tool/multi_sequence_extractor.rb
@@ -536,7 +536,7 @@ module Evoruby
       rescue Exception => e
         Util.fatal_error( PRG_NAME, "error: " + e.to_s )
       end
-      if @seqs <= 600000
+      if @seqs <= 400000
         @file_to_msa[ input ] = msa
         @seqs += msa.get_number_of_seqs
         puts "   total seqs in memory: " + @seqs.to_s
