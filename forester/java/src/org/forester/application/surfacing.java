fd847171bd527f7e119f04145ce3abd8caa88803
cmzmasek@gmail.com <cmzmasek@gmail.com@ca865154-3058-d1c3-3e42-d8f55a55bdbd>
2014-03-11 23:04:39 +0000

    inprogress

--- forester/java/src/org/forester/application/surfacing.java
+++ forester/java/src/org/forester/application/surfacing.java
@@ -2196,17 +2196,9 @@ public class surfacing {
         System.out.println( DA_ANALYSIS_OPTION + ": to perform DA analysis" );
         System.out.println( PERFORM_DOMAIN_LENGTH_ANALYSIS_OPTION + ": to perform domain length analysis" );
         System.out.println();
-        System.out.println( "Example 1: surfacing -p2g=pfam2go_2012_02_07.txt -dufs -cos=Pfam_260_NC1"
-                + " -no_eo -mo=0 -genomes=eukaryotes.txt -out_dir=out -o=o "
-                + " -species_tree=tol.xml -obo=gene_ontology_2012_02_07.obo -pos_filter=f.txt -all_prot" );
-        System.out.println();
-        System.out.println( "Example 2: surfacing -detail=punctilious -o=TEST.html -pwc=TEST"
-                + " -cos=Pfam_ls_22_TC2 -p2g=pfam2go -obo=gene_ontology_edit.obo "
-                + "-dc_sort=dom -ignore_with_self -no_singles -ie=0.001 -mo=1 -no_eo -genomes=eukaryotes.txt "
-                + "-ds_output=detailed_html -scoring=domains -sort=alpha " );
         System.out.println();
         System.out
-                .println( "Example 3: surfacing -p2g=pfam2go_130621.txt -obo=gene_onotology_130621.obo -species_tree=tol_155.xml -last -detail=punctilious -ignore_viral_ids -no_eo -ie=1 -dufs -genomes=genomes_all.txt -pos_filter=tf_1.txt -all_prot -all_prot_e=1 -out_dir=_tf1_e1_ape1 -o=tf1_e1_ape1" );
+                .println( "Example: surfacing -p2g=pfam2go_130621.txt -obo=gene_onotology_130621.obo -species_tree=tol_155.xml -last -detail=punctilious -ignore_viral_ids -no_eo -ie=0.1 -dufs -genomes=genomes_all.txt -pos_filter=tf_1.txt -all_prot -all_prot_e=0.1 -out_dir=_tf1_e01_ape01 -o=tf1_e01_ape01" );
         System.out.println();
     }
 }
