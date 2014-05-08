35691615541bd77a757568ce4628f8fcfcb03055
cmzmasek <cmzmasek@ca865154-3058-d1c3-3e42-d8f55a55bdbd>
2014-05-08 18:57:33 +0000

    in progress

--- forester/java/src/org/forester/application/msa_compactor.java
+++ forester/java/src/org/forester/application/msa_compactor.java
@@ -73,8 +73,8 @@ public class msa_compactor {
     final static private String       DO_NOT_NORMALIZE_FOR_EFF_LENGTH_OPTION = "nn";
     final static private String       PRG_NAME                               = "msa_compactor";
     final static private String       PRG_DESC                               = "multiple sequence aligment compactor";
-    final static private String       PRG_VERSION                            = "0.2";
-    final static private String       PRG_DATE                               = "140428";
+    final static private String       PRG_VERSION                            = "0.3";
+    final static private String       PRG_DATE                               = "140508";
     final static private String       E_MAIL                                 = "czmasek@sanfordburham.org";
     final static private String       WWW                                    = "https://sites.google.com/site/cmzmasek/home/software/forester";
 
