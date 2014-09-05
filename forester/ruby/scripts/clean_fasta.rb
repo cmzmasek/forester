47de35c937e55cccd5d474f0da468e18577ee60b
cmzmasek@gmail.com <cmzmasek@gmail.com@ca865154-3058-d1c3-3e42-d8f55a55bdbd>
2014-09-05 23:56:10 +0000

    in progress

---
+++ forester/ruby/scripts/clean_fasta.rb
@@ -0,0 +1,17 @@
+require 'fileutils'
+
+infile = ARGV[ 0 ]
+
+File.open(infile, 'r') do |f|
+   while line = f.gets
+       if line =~ /(>.+)~.+/
+          line = $1
+       elsif line =~ /(>.+)|NAME=.+/
+           line = $1
+       end
+       puts s
+   end
+end
+
+
+
