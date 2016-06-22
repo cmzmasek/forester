b528989f688291ba7615867e5322090ec42096d8
cmzmasek <cmzmasek@yahoo.com>
2016-06-21 18:34:02 -0700

    needed for https for treebase

---
+++ forester/java/src/org/forester/util/TrustManager.java
@@ -0,0 +1,44 @@
+package org.forester.util;
+
+import java.io.IOException;
+import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.X509TrustManager;
+
+public class TrustManager implements X509TrustManager {
+    
+    @Override
+    public X509Certificate[] getAcceptedIssuers() {
+        return null;
+    }
+
+    @Override
+    public void checkServerTrusted(X509Certificate[] paramArrayOfX509Certificate, String paramString)
+        throws CertificateException {
+    }
+
+    @Override
+    public void checkClientTrusted(X509Certificate[] paramArrayOfX509Certificate, String paramString)
+        throws CertificateException {
+    }
+    
+    public final static HttpsURLConnection makeHttpsURLConnection( final URL url ) throws NoSuchAlgorithmException,
+                                                                                        IOException,
+                                                                                        KeyManagementException {
+        
+        final SSLContext ctx = SSLContext.getInstance("TLS");
+        ctx.init(null, new TrustManager[] { new TrustManager() }, null);
+        SSLContext.setDefault(ctx);
+        
+        final HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+        return con;
+        
+    }
+  
+}
