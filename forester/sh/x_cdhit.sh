1c2e85eda9e20c26e944ef960f5d62af78ef257e
Christian Zmasek <chris.zma@outlook.com>
2019-06-26 17:39:58 -0700

    in progress

---
+++ forester/sh/x_cdhit.sh
@@ -0,0 +1,34 @@
+CD_HIT="/home/zma/SOFTWARE/CDHIT/cdhit-4.6.6/cd-hit"
+CUTOFF=0.95
+
+indir=$1
+outdir=$2
+
+if [ ! -d "$outdir" ]; then
+    mkdir $outdir
+fi
+
+if [ ! -z "$(ls -A $outdir)" ]; then
+    echo "Output directory is not empty"
+    exit 0
+fi
+
+if [ -z "$(ls -A $indir)" ]; then
+    echo "Input directory is empty"
+    exit 0
+fi
+
+fasta_re="/(.+)\.fasta$"
+
+for i in $indir/* 
+do
+    if test -f "$i" 
+    then
+        if [[ $i =~ $fasta_re ]]
+        then
+            name=${BASH_REMATCH[1]}
+            echo "Executing: $CD_HIT -c $CUTOFF -i $indir/$name.fasta -o $outdir/$name.fasta:"
+            $CD_HIT -c $CUTOFF -i $indir/$name.fasta -o $outdir/$name.fasta
+        fi
+    fi
+done
--- forester/sh/x_tap.sh
+++ forester/sh/x_tap.sh
@@ -16,5 +16,3 @@ do
         fi
     fi
 done
-
-I had high hopes for this movie. That is why I watched it on Thursday night. Unfortunately, it turned out a complete disappointment. The story is boring and predictable, the action sequences amateurish in their execution, and the humor falls flat. The worst of all is the main actress, though. What were they thinking by casting her? She does not fit the role at all! She has zero charisma and actually seemed bored during filming.
