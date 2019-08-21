0a2aab98f2f04b488dee8b80a4af72fa1a97a3e2
Christian Zmasek <chris.zma@outlook.com>
2019-08-21 15:45:50 -0700

    updated

--- forester/python/surfacing_table_reformat.py
+++ forester/python/surfacing_table_reformat.py
@@ -2,31 +2,52 @@ import csv
 
 name_col = 1
 da_col = 0
-ids_col = 3
+acc_col = 3
 
-infile = 'DA_SPECIES_IDS_MAP_mod.csv'
+acc_to_da = dict()
+acc_to_acc_name_da = dict()
+
+infile = 'DA_SPECIES_IDS_MAP.txt'
 outfile = 'ID_SOGNAME_DA_MAP.txt'
 
 with open(infile, 'r') as in_table, open(outfile, 'w') as out_table:
     reader = csv.reader(in_table, delimiter='\t')
 
-    out_table.write('#ID(GENBANK OR VIPR)\tSOG NAME\tDOMAIN ARCHITECTURE\n')
+    out_table.write('#ACC\tSOG NAME\tDOMAIN ARCHITECTURE\n')
 
     for row in reader:
         name = row[name_col]
         da = row[da_col]
-        ids = row[ids_col]
+        accs = row[acc_col]
 
         name = name.strip('\"')
         da = da.strip('\"')
-        ids = ids.strip('\"')
+        accs = accs.strip('\"')
 
-        idss = ids.split(',')
-        print(idss)
-        #   if len(idss) > 0:
-        for identifier in idss:
-            out_table.write(f'{identifier}\t{name}\t{da}\n')
+        accessors = accs.split(',')
+      
+        if len(accessors) > 0:
+            for acc in accessors:
+                new_acc_na_da_str =  acc + '\t' + name + '\t' + da
+                if acc in acc_to_da:
+                    print('multiple: ' + acc_to_acc_name_da[ acc ])
+                    print('        : ' + new_acc_na_da_str)
+                    if da != acc_to_da[ acc ]:
+                          if len(da) > len( acc_to_da[ acc ] ):
+                               acc_to_da[ acc ] = da
+                               acc_to_acc_name_da[ acc ] =  new_acc_na_da_str
+                    print('keeping : ' + acc_to_acc_name_da[ acc ])     
+                    print()
+                else:
+                    acc_to_da[ acc ] = da
+                    acc_to_acc_name_da[ acc ] =  new_acc_na_da_str
+            
+                
         else:
-            print(f"warning: no mapping for {name}")
+            print(f"Error: no mapping for {name}")
+            quit()
 
+    for acc in acc_to_acc_name_da:
+        out_table.write(f'{acc_to_acc_name_da[acc]}\n')
 out_table.close()
+print('wrote: ' + outfile )
