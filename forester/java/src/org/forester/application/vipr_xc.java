266fd419319c561624843025c1f0584296517d00
Christian Zmasek <cmzmasek@users.noreply.github.com>
2024-05-16 11:21:33 -0700

    version 1.0.0

--- forester/java/src/org/forester/application/vipr_xc.java
+++ forester/java/src/org/forester/application/vipr_xc.java
@@ -1,4 +1,3 @@
-
 package org.forester.application;
 
 import org.forester.io.parsers.PhylogenyParser;
@@ -24,14 +23,14 @@ import java.util.TreeMap;
 
 public class vipr_xc {
 
+    private static final String PRG_DATE = "2024-05-16";
+    private static final String PRG_VERSION = "1.0.0";
     private final static String PRG_NAME = "vipr_xc";
     private static final String XSD_STRING = "xsd:string";
-
-
     private static final String H5_CLADE = "vipr:H5_clade";
 
     public static void main(final String args[]) {
-
+        ForesterUtil.printProgramInformation(PRG_NAME, PRG_VERSION, PRG_DATE);
         if (args.length != 3) {
             System.out.println("\nWrong number of arguments, expected: <intree> <clade map> <outtree>\n");
             System.exit(-1);
@@ -62,17 +61,16 @@ public class vipr_xc {
         try {
             isolate_to_clade = BasicTableParser.parse(clade_infile, '\t', false, false);
         } catch (final Exception e) {
-            ForesterUtil.fatalError(PRG_NAME,
-                    "failed to read [" + clade_infile + "] [" + e.getMessage() + "]");
+            ForesterUtil.fatalError(PRG_NAME, "failed to read [" + clade_infile + "] [" + e.getMessage() + "]");
         }
 
 
         final SortedMap<String, String> isolate_to_clade_map = new TreeMap<String, String>();
         for (int row = 0; row < isolate_to_clade.getNumberOfRows(); ++row) {
-            final String key = (String) isolate_to_clade.getValue(0, row);
+            final String key = isolate_to_clade.getValue(0, row);
             final String value = isolate_to_clade.getValue(1, row);
             if ((key != null) && (value != null)) {
-                String x[] = key.split("\\|");
+                final String x[] = key.split("\\|");
                 final String key_acc = x[x.length - 1];
                 if (isolate_to_clade_map.containsKey(key_acc)) {
                     throw new IllegalArgumentException("attempt to use non-unique table value as key [" + key_acc + "]");
@@ -81,11 +79,14 @@ public class vipr_xc {
             }
         }
 
-
         final List<PhylogenyNode> ext_nodes = p.getExternalNodes();
+        int ext_nodes_count = 0;
+        int mapped = 0;
+        int not_mapped = 0;
         for (final PhylogenyNode ext_node : ext_nodes) {
+            ++ext_nodes_count;
             final String name = ext_node.getName();
-            String x[] = name.split("\\|");
+            final String x[] = name.split("\\|");
             final String name_acc = x[x.length - 1];
             if (!ForesterUtil.isEmpty(name)) {
                 if (isolate_to_clade_map.containsKey(name_acc)) {
@@ -93,9 +94,13 @@ public class vipr_xc {
                     if (clade.length() > 0 && clade.indexOf("cannot") < 0) {
                         PropertiesList custom_data = ext_node.getNodeData().getProperties();
                         custom_data.addProperty(new Property(H5_CLADE, clade, "", XSD_STRING, AppliesTo.NODE));
+                        ++mapped;
+                    } else {
+                        ++not_mapped;
                     }
                 } else {
-                    System.out.println("not found in map: " + name);
+                    System.out.println("Error: not found in map: " + name);
+                    System.exit(-1);
                 }
             }
         }
@@ -108,5 +113,15 @@ public class vipr_xc {
             System.out.println("\nFailure to write output [" + e.getMessage() + "]\n");
             System.exit(-1);
         }
+        System.out.println();
+        System.out.println("External nodes                    : " + ext_nodes_count);
+        System.out.println("External nodes with clade added   : " + mapped);
+        System.out.println("External nodes without clade added: " + not_mapped);
+        System.out.println();
+        System.out.println();
+        System.out.println("[" + PRG_NAME + "] wrote: [" + outfile + "]");
+        System.out.println("[" + PRG_NAME + "] OK");
+        System.out.println();
+
     }
 }
