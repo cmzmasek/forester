4f7857dfb903b14c11c9e5303fb8a6637d4135d5
Christian Zmasek <cmzmasek@users.noreply.github.com>
2024-05-28 20:08:31 -0700

    in progress

--- forester/java/src/org/forester/application/phyloxml2nh.java
+++ forester/java/src/org/forester/application/phyloxml2nh.java
@@ -19,18 +19,27 @@ import org.forester.util.ForesterUtil;
 
 public class phyloxml2nh {
 
-    private static final String PRG_DATE    = "2023-08-18";
-    private static final String PRG_VERSION = "1.0.0";
+    private static final String PRG_DATE    = "2024-05-28";
+    private static final String PRG_VERSION = "1.0.1";
     private static final String PRG_NAME    = "phyloxml2nh";
     public static void main( final String args[] ) {
-        if ( ( args.length != 3 ) ) {
+        if ( ( args.length != 3 && args.length != 2 ) ) {
             System.out.println( PRG_NAME + ": Wrong number of arguments.\n" );
-            System.out.println( "Usage: " + PRG_NAME + " <ref> <in-tree>< out-tree>\n" );
+            System.out.println( "Usage: " + PRG_NAME + " <in-tree> <out-tree> [ref]\n" );
             System.exit( -1 );
         }
-        final String ref = args[ 0 ];
-        final File intree = new File( args[ 1 ] );
-        final File outtree = new File( args[ 2 ] );
+
+        final File intree = new File( args[ 0 ] );
+        final File outtree = new File( args[ 1 ] );
+
+        final String ref;
+        if  ( args.length == 3 ) {
+            ref = args[ 1 ];
+        }
+        else {
+            ref = null;
+        }
+
         final String e1 = ForesterUtil.isReadableFile( intree );
         if ( !ForesterUtil.isEmpty( e1 ) ) {
             ForesterUtil.fatalError( PRG_NAME, e1 );
@@ -48,22 +57,25 @@ public class phyloxml2nh {
         catch ( final Exception e ) {
             ForesterUtil.fatalError( PRG_NAME, "Could not read \"" + intree + "\" [" + e.getMessage() + "]" );
         }
-        int counter = 0;
-        for( final PhylogenyNodeIterator iter = p.iteratorPostorder(); iter.hasNext(); ) {
-            final PhylogenyNode node = iter.next();
-            final PropertiesList custom_data = node.getNodeData().getProperties();
-            if ( custom_data != null ) {
-                final List<Property> props = custom_data.getProperties( ref );
-                if ( ( props.size() > 0 ) && ( props.get( 0 ) != null ) ) {
-                    node.setName( props.get( 0 ).getValue() );
-                    ++counter;
+
+        if ( ref != null ) {
+            int counter = 0;
+            for (final PhylogenyNodeIterator iter = p.iteratorPostorder(); iter.hasNext(); ) {
+                final PhylogenyNode node = iter.next();
+                final PropertiesList custom_data = node.getNodeData().getProperties();
+                if (custom_data != null) {
+                    final List<Property> props = custom_data.getProperties(ref);
+                    if ((props.size() > 0) && (props.get(0) != null)) {
+                        node.setName(props.get(0).getValue());
+                        ++counter;
+                    }
                 }
             }
+            System.out.println("Replaced " + counter + " names");
         }
-        System.out.println( "Replaced " + counter + " names" );
         final PhylogenyWriter writer = new PhylogenyWriter();
         try {
-            writer.toNewHampshire( p, true, outtree );
+            writer.toNewHampshire( p, true, PhylogenyNode.NH_CONVERSION_SUPPORT_VALUE_STYLE.AS_INTERNAL_NODE_NAMES,outtree );
         }
         catch ( final IOException e ) {
             ForesterUtil.fatalError( PRG_NAME, e.toString() );
