2f7968d194b6f9113d238729fb8a6090a9a5c7c2
Christian Zmasek <cmzmasek@users.noreply.github.com>
2025-06-11 15:37:51 -0700

    Automated reference tree inference and annotation, in progress

--- forester/sh/ref_phylo_pl.sh
+++ forester/sh/ref_phylo_pl.sh
@@ -1,35 +1,53 @@
-VERSION="1.0.0"
+VERSION="0.0.1"
 CLEAN_FASTA="python /Users/czmasek/Dropbox/PROG/PYTHON/PYCHARM_PROJECTS/TWO/clean_fasta.py"
 CDHIT="/Users/czmasek/anaconda3/bin/cd-hit"
 MK_TREE="bash /Users/czmasek/IdeaProjects/forester/forester/sh/mktree.sh"
 VIPR_X="java -Xmx8048m -cp /Users/czmasek/IdeaProjects/forester/forester/java/forester.jar org.forester.application.vipr_x6"
 
-
-genomes_file=$1
-annotation_file=$2
-seq_type=$3
-minimal_length_a=$4
-minimal_length_b=$5
-char_ratio=$6
-clustering_cutoff_a=$7
-clustering_cutoff_b=$8
-mafft_opts=$9
-tree_inference_opt=$10
-date_str=$
-tree_a_desc=$
-tree_b_desc=$
-tree_a_output_name=
-tree_b_output_name=
-workdir=$
-
-if [ "$#" -ne  ]; then
+if [ "$#" -ne 8 ]; then
   echo "Usage: " >&2
   echo "    Example 1: ref_phylo_pl.sh ref_phylo_pl.sh BVBRC_1900_2025.fasta ../JUNE_10_2025/BVBRC_genome.txt na 12000 450 0.999 0.95 workdir"
-   exit 1
+  exit 1
 fi
 
+options_file=$1
+genomes_file=$2
+annotation_file=$3
+tree_a_desc=$4
+tree_b_desc=$5
+tree_a_output_name=$6
+tree_b_output_name=$7
+workdir=$8
+
+if [ ! -f "$options_file" ]; then
+  echo "$options_file does not exist"
+  exit -1
+fi
+
+seq_type="?"
+minimal_length_a=-1
+char_ratio=-1
+for line in $(cat $options_file); do
+  echo $line
+  if [[ "$line" =~ SEQ_TYPE=(.+) ]]; then
+    seq_type="${BASH_REMATCH[1]}"
+  fi
+  if [[ "$line" =~ MINIMAL_LENGTH_A=(.+) ]]; then
+    minimal_length_a="${BASH_REMATCH[1]}"
+  fi
+  if [[ "$line" =~ MINIMAL_LENGTH_B=(.+) ]]; then
+    minimal_length_b="${BASH_REMATCH[1]}"
+  fi
+  if [[ "$line" =~ CHAR_RATIO=(.+) ]]; then
+    char_ratio="${BASH_REMATCH[1]}"
+  fi
+
+done
+
 echo "Version             : $VERSION"
-echo "Genomes file        : $genomes_file"
+echo "Options file        : $options_file"
+echo "Genomes file A      : $genomes_file_a"
+echo "Genomes file B      : $genomes_file_b"
 echo "Annotations file    : $annotation_file"
 echo "Sequence type       : $seq_type"
 echo "Min length A        : $minimal_length_a"
@@ -37,9 +55,17 @@ echo "Min length B        : $minimal_length_b"
 echo "Ratio               : $char_ratio"
 echo "Clustering cutoff A : $clustering_cutoff_a"
 echo "Clustering cutoff B : $clustering_cutoff_b"
+echo "MAFFT options A     : $mafft_opts_a"
+echo "MAFFT options B     : $mafft_opts_b"
+echo "MSA pro options A   : $msa_pro_opts_a"
+echo "MSA pro options B   : $msa_pro_opts_b"
+echo "Tree inf options A  : $tree_inference_opts_a"
+echo "Tree inf options B  : $tree_inference_opts_b"
 echo "Date string         : $date_str"
 echo "Tree desc A         : $tree_a_desc"
 echo "Tree desc B         : $tree_b_desc"
+echo "Tree A output name  : $tree_a_output_name"
+echo "Tree B output name  : $tree_b_output_name"
 echo "Work dir            : $workdir"
 echo ""
 echo ""
@@ -53,30 +79,47 @@ if [ ! -z "$(ls -A $workdir)" ]; then
   exit -1
 fi
 
-if [ ! -z "$(ls -A $workdir)" ]; then
-  echo "Tree already exists"
+if [ -f "$tree_a_output_name" ]; then
+  echo "Tree $tree_a_output_name already exists"
   exit -1
 fi
 
-if [ ! -z "$(ls -A $workdir)" ]; then
-  echo "Tree already exists"
+if [ -f "$tree_b_output_name" ]; then
+  echo "Tree $tree_b_output_name already exists"
   exit -1
 fi
 
-$CLEAN_FASTA -ml $minimal_length_a -r $char_ratio -t $seq_type $genomes_file $workdir/genomes_clean_a.fasta
+if [ ! -f "$genomes_file_a" ]; then
+  echo "$genomes_file_a does not exist"
+  exit -1
+fi
 
-$CLEAN_FASTA -ml $minimal_length_b -r $char_ratio -t $seq_type $genomes_file $workdir/genomes_clean_b.fasta
+if [ ! -f "$genomes_file_b" ]; then
+  echo "$genomes_file_b does not exist"
+  exit -1
+fi
+
+if [ ! -f "$annotation_file" ]; then
+  echo "$annotation_file does not exist"
+  exit -1
+fi
+
+$CLEAN_FASTA -ml $minimal_length_a -r $char_ratio -t $seq_type $genomes_file_a $workdir/genomes_clean_a.fasta
+$CLEAN_FASTA -ml $minimal_length_b -r $char_ratio -t $seq_type $genomes_file_b $workdir/genomes_clean_b.fasta
+
+#only cluster if cutoot between 0 and 1:
 
 $CDHIT -c $clustering_cutoff_a -d 0 -g 1 -i $workdir/genomes_clean_a.fasta -o $workdir/genomes_clean_a_cdhit.fasta
-
 $CDHIT -c $clustering_cutoff_b -d 0 -g 1 -i $workdir/genomes_clean_b.fasta -o $workdir/genomes_clean_b_cdhit.fasta
 
-mkdir $workdir/trees
+mkdir $workdir/trees_a
+mkdir $workdir/trees_b
 
-cp $workdir/genomes_clean_a_cdhit.fasta $workdir/trees/
-cp $workdir/genomes_clean_b_cdhit.fasta $workdir/trees/
+cp $workdir/genomes_clean_a_cdhit.fasta $workdir/trees_a/
+cp $workdir/genomes_clean_b_cdhit.fasta $workdir/trees_b/
 
-$MK_TREE "--auto --thread 8" "-rr=0.2 -rsl=450" "-B100Zxm" .fasta $workdir/trees/
+$MK_TREE "$mafft_opts_a" "$msa_pro_opts_a" "-$tree_inference_opts_a" .fasta $workdir/trees_a/
+$MK_TREE "$mafft_opts_b" "$msa_pro_opts_b" "-$tree_inference_opts_b" .fasta $workdir/trees_b/
 
-$VIPR_X $workdir/trees/deco/genomes_clean_a_cdhit_ni_mafft_tree_raxml_d.xml $annotation_file measles_virus_compl_genome_cdhit099.xml "M. hominis genome 2025-XX-XX cdhit0.99 RAxML GTR G4"
-
+$VIPR_X $workdir/trees_a/deco/genomes_clean_a_cdhit_ni_mafft_tree_raxml_d.xml $annotation_file $tree_a_output_name "$tree_a_desc"
+$VIPR_X $workdir/trees_b/deco/genomes_clean_b_cdhit_ni_mafft_tree_raxml_d.xml $annotation_file $tree_b_output_name "$tree_b_desc"
