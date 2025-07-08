69f3aa5c0f0d89d5d2681eda186c77d7bb9f540d
Christian Zmasek <cmzmasek@users.noreply.github.com>
2025-07-07 17:36:52 -0700

    v 0.0.1 == automation of ref tree inference

--- forester/sh/ref_phylo_pl.sh
+++ forester/sh/ref_phylo_pl.sh
@@ -1,23 +1,38 @@
 VERSION="0.0.1"
-CLEAN_FASTA="python /Users/czmasek/Dropbox/PROG/PYTHON/PYCHARM_PROJECTS/TWO/clean_fasta.py"
+CLEAN_FASTA="python3 /Users/czmasek/Dropbox/PROG/PYTHON/PYCHARM_PROJECTS/TWO/clean_fasta.py"
 CDHIT="/Users/czmasek/anaconda3/bin/cd-hit"
 MK_TREE="bash /Users/czmasek/IdeaProjects/forester/forester/sh/mktree.sh"
 VIPR_X="java -Xmx8048m -cp /Users/czmasek/IdeaProjects/forester/forester/java/forester.jar org.forester.application.vipr_x6"
 
-if [ "$#" -ne 8 ]; then
+# Example options file:
+# SEQ_TYPE=na
+# MINIMAL_LENGTH_A=450
+# MINIMAL_LENGTH_B=12000
+# CHAR_RATIO=0.999
+# CLUSTERING_CUTOFF_A=0.999
+# CLUSTERING_CUTOFF_B=0.99
+# MAFFT_OPTS_A=--auto --thread 8
+# MAFFT_OPTS_B=--auto --thread 8
+# MSA_PRO_OPTS_A=-rr=0.2 -rsl=450
+# MSA_PRO_OPTS_B=-rr=0.2 -rsl=450
+# TREE_INFERENCE_OPTS_A=-B100Zxm
+# TREE_INFERENCE_OPTS_B=-B100Zxm
+# TREE_A_DESC=this is tree a
+# TREE_B_DESC=this is tree b
+
+if [ "$#" -ne 7 ]; then
   echo "Usage: " >&2
-  echo "    Example 1: ref_phylo_pl.sh ref_phylo_pl.sh BVBRC_1900_2025.fasta ../JUNE_10_2025/BVBRC_genome.txt na 12000 450 0.999 0.95 workdir"
+  echo "    Example 1: ref_phylo_pl.sh options BVBRC_1900_2025.fasta ../JUNE_10_2025/BVBRC_genome.txt 2025-06-6 tree_a tree_b workdir"
   exit 1
 fi
 
 options_file=$1
 genomes_file=$2
 annotation_file=$3
-tree_a_desc=$4
-tree_b_desc=$5
-tree_a_output_name=$6
-tree_b_output_name=$7
-workdir=$8
+date_str=$4
+tree_a_output_name=$5
+tree_b_output_name=$6
+workdir=$7
 
 if [ ! -f "$options_file" ]; then
   echo "$options_file does not exist"
@@ -26,9 +41,18 @@ fi
 
 seq_type="?"
 minimal_length_a=-1
+minimal_length_b=-1
 char_ratio=-1
-for line in $(cat $options_file); do
-  echo $line
+clustering_cutoff_a=-1
+clustering_cutoff_b=-1
+mafft_opts_a="?"
+mafft_opts_b="?"
+msa_pro_opts_a="?"
+msa_pro_opts_b="?"
+tree_inference_opts_a="?"
+tree_inference_opts_b="?"
+
+while IFS= read -r line; do
   if [[ "$line" =~ SEQ_TYPE=(.+) ]]; then
     seq_type="${BASH_REMATCH[1]}"
   fi
@@ -41,13 +65,41 @@ for line in $(cat $options_file); do
   if [[ "$line" =~ CHAR_RATIO=(.+) ]]; then
     char_ratio="${BASH_REMATCH[1]}"
   fi
-
-done
+  if [[ "$line" =~ CLUSTERING_CUTOFF_A=(.+) ]]; then
+    clustering_cutoff_a="${BASH_REMATCH[1]}"
+  fi
+  if [[ "$line" =~ CLUSTERING_CUTOFF_B=(.+) ]]; then
+    clustering_cutoff_b="${BASH_REMATCH[1]}"
+  fi
+  if [[ "$line" =~ MAFFT_OPTS_A=(.+) ]]; then
+    mafft_opts_a="${BASH_REMATCH[1]}"
+  fi
+  if [[ "$line" =~ MAFFT_OPTS_B=(.+) ]]; then
+    mafft_opts_b="${BASH_REMATCH[1]}"
+  fi
+  if [[ "$line" =~ MSA_PRO_OPTS_A=(.+) ]]; then
+    msa_pro_opts_a="${BASH_REMATCH[1]}"
+  fi
+  if [[ "$line" =~ MSA_PRO_OPTS_B=(.+) ]]; then
+    msa_pro_opts_b="${BASH_REMATCH[1]}"
+  fi
+  if [[ "$line" =~ TREE_INFERENCE_OPTS_A=(.+) ]]; then
+    tree_inference_opts_a="${BASH_REMATCH[1]}"
+  fi
+  if [[ "$line" =~ TREE_INFERENCE_OPTS_B=(.+) ]]; then
+    tree_inference_opts_b="${BASH_REMATCH[1]}"
+  fi
+  if [[ "$line" =~ TREE_A_DESC=(.+) ]]; then
+    tree_a_desc="${BASH_REMATCH[1]}"
+  fi
+  if [[ "$line" =~ TREE_B_DESC=(.+) ]]; then
+    tree_b_desc="${BASH_REMATCH[1]}"
+  fi
+done <$options_file
 
 echo "Version             : $VERSION"
 echo "Options file        : $options_file"
-echo "Genomes file A      : $genomes_file_a"
-echo "Genomes file B      : $genomes_file_b"
+echo "Genomes file A      : $genomes_file"
 echo "Annotations file    : $annotation_file"
 echo "Sequence type       : $seq_type"
 echo "Min length A        : $minimal_length_a"
@@ -89,13 +141,8 @@ if [ -f "$tree_b_output_name" ]; then
   exit -1
 fi
 
-if [ ! -f "$genomes_file_a" ]; then
-  echo "$genomes_file_a does not exist"
-  exit -1
-fi
-
-if [ ! -f "$genomes_file_b" ]; then
-  echo "$genomes_file_b does not exist"
+if [ ! -f "$genomes_file" ]; then
+  echo "$genomes_file does not exist"
   exit -1
 fi
 
@@ -104,10 +151,10 @@ if [ ! -f "$annotation_file" ]; then
   exit -1
 fi
 
-$CLEAN_FASTA -ml $minimal_length_a -r $char_ratio -t $seq_type $genomes_file_a $workdir/genomes_clean_a.fasta
-$CLEAN_FASTA -ml $minimal_length_b -r $char_ratio -t $seq_type $genomes_file_b $workdir/genomes_clean_b.fasta
+$CLEAN_FASTA -ml $minimal_length_a -r $char_ratio -t $seq_type $genomes_file $workdir/genomes_clean_a.fasta
+$CLEAN_FASTA -ml $minimal_length_b -r $char_ratio -t $seq_type $genomes_file $workdir/genomes_clean_b.fasta
 
-#only cluster if cutoot between 0 and 1:
+#only cluster if cutoff between 0 and 1:
 
 $CDHIT -c $clustering_cutoff_a -d 0 -g 1 -i $workdir/genomes_clean_a.fasta -o $workdir/genomes_clean_a_cdhit.fasta
 $CDHIT -c $clustering_cutoff_b -d 0 -g 1 -i $workdir/genomes_clean_b.fasta -o $workdir/genomes_clean_b_cdhit.fasta
